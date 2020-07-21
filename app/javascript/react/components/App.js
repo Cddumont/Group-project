5ed4c172c62d6999b454c751e1d3f6f0a921c30b
NickDev00 <Nashley3785@gmail.com>
2020-07-21 14:50:30 -0400

    Linked homes/index.html.erb to top level react component, and added browserrouter to app.js

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { BrowserRouter, Switch } from "react-router-dom"
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  return (
+    <BrowserRouter>
+      <Switch> 
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
