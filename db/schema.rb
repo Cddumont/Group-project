0ab6f17f4f9f3000ddd305a97810c68ed32fbdd5
Andrew Higgins <andrewhiggins27@gmail.com>
2020-07-22 15:34:07 -0400

    Devise updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_143246) do
+ActiveRecord::Schema.define(version: 2020_07_22_190412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_143246) do
     t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "videogames", force: :cascade do |t|
