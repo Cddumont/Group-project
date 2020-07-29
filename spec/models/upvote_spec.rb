72d24194baed2a3794f61e6c37c42fcdffcf8e18
cddumont <cddumont@gmail.com>
2020-07-29 16:09:02 -0400

    upvotes and downvotes

--- spec/models/upvote_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-# RSpec.describe Upvote, type: :model do
-#   describe 'validations' do
-#     it { should validate_uniqueness_of(:user).ignoring_case_sensitivity}
-#     it { should validate_uniqueness_of(:review).ignoring_case_sensitivity}
-#   end
-# end
