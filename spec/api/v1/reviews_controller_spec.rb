b806e08a818ec80144deaceb651c900d99b6e438
Sen <senboyer90launch@gmail.com>
2020-07-29 15:51:20 -0400

    work in progress

--- spec/api/v1/reviews_controller_spec.rb
+++ spec/api/v1/reviews_controller_spec.rb
@@ -61,5 +61,33 @@ RSpec.describe Api::V1::ReviewsController, type: :controller do
       expect(returned_json).to_not be_kind_of(Array)
       expect(returned_json["submitted"]).to eq false
     end
+    RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
+end
+
+RSpec.describe API::V1::ReviewsController, type: :controller do
+  describe "GET#index" do
+    it "should return information about the current user" do
+      user = FactoryBot.create(:user)
+      sign_in user
+      get :index
+
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(returned_json["user"]["first_name"]).to eq user.first_name
+      expect(returned_json["user"]["last_name"]).to eq user.last_name
+    end
   end
+
+  scenario 'specify valid credentials' do
+    visit new_user_session_path
+  
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+  
+    click_button 'Log in'
+    end
+  end
+end
 end
--- spec/support/devise_controller_spec.rb
+++ spec/support/devise_controller_spec.rb
@@ -1,3 +1,33 @@
+require "rails_helper"
+
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
 end
+
+RSpec.describe ReviewsController, type: :controller do
+  describe "GET#index" do
+    it "should return information about the current user" do
+      user = FactoryBot.create(:user)
+      sign_in user
+      get :index
+
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(returned_json["user"]["first_name"]).to eq user.first_name
+      expect(returned_json["user"]["last_name"]).to eq user.last_name
+    end
+  end
+
+  scenario 'specify valid credentials' do
+    visit new_user_session_path
+  
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+  
+    click_button 'Log in'
+  end
+
+
+end
+
