7ecab23541892144d01cef96c9ad8d53e8274696
cddumont <cddumont@gmail.com>
2020-07-29 15:08:48 -0400

    not real

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_07_28_182323) do
-=======
-ActiveRecord::Schema.define(version: 2020_07_27_174717) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
