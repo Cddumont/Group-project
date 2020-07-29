c3a6b689028131d179ea22f7576488d3939ba728
Andrew Higgins <andrewhiggins27@gmail.com>
2020-07-29 14:18:38 -0400

    added tests

--- spec/api/v1/downvotes_controller_spec.rb
+++ spec/api/v1/downvotes_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::UpvotesController, type: :controller do
+RSpec.describe Api::V1::DownvotesController, type: :controller do
   describe "POST#create" do
     let!(:videogame1) {Videogame.create(name:"Spyro")}
     let!(:review1) {Review.create(rating: 3, videogame: videogame1)}
@@ -11,23 +11,23 @@ RSpec.describe Api::V1::UpvotesController, type: :controller do
       
       expect(returned_json['errors']).to eq("User must exist")
     end
-    it "returns error message if user has already upvoted" do
+    it "returns error message if user has already downvoted" do
       user = FactoryBot.create(:user)
       sign_in user
-      Upvote.create(user: user, review: review1)
+      Downvote.create(user: user, review: review1)
       
       post :create, params: {review_id: review1.id}
       returned_json = JSON.parse(response.body)
 
       expect(returned_json["errors"]).to eq("Review has already been taken and User has already been taken")
     end
-    it "if user is signed in it creates an upvote" do
+    it "if user is signed in it creates a downvote" do
       user = FactoryBot.create(:user)
       sign_in user
 
       post :create, params: {review_id: review1.id}
 
-      expect(user.upvotes.count).to eq(1)
+      expect(user.downvotes.count).to eq(1)
     end
     it "returns json of all the reviews" do
       user = FactoryBot.create(:user)
@@ -40,15 +40,15 @@ RSpec.describe Api::V1::UpvotesController, type: :controller do
       expect(returned_json["reviews"]).to be_kind_of(Array)
       expect(returned_json["reviews"].first["rating"]).to eq(review1.rating)
     end
-    it "if the user already downvoted the review, it destroys the downvote" do
+    it "if the user already upvoted the review, it destroys the upvote" do
       user = FactoryBot.create(:user)
-      Downvote.create(user: user, review: review1)
-      downvoteCount = user.downvotes.count
+      Upvote.create(user: user, review: review1)
+      upvoteCount = user.upvotes.count
       sign_in user
 
       post :create, params: {review_id: review1.id}
 
-      expect(user.downvotes.count).to eq(downvoteCount - 1)
+      expect(user.upvotes.count).to eq(upvoteCount - 1)
     end
   end
 end
